e803d66f69ad1b3c45f34470bb09aad5773d8181
Paul Mongold <pmongold@gmail.com>
2013-05-20 22:58:11 -0700

    toro: reset to b1 for v3.2 builds and cleanup
    Change-Id: I55f800fd65a1ea4de28831104940f2f4fd3ba941

--- ev.mk
+++ ev.mk
@@ -16,17 +16,15 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysid BUILD_FINGERPRINT="google/mys
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Primo
-PRODUCT_VERSION_DEVICE_SPECIFIC := b2
+PRODUCT_VERSION_DEVICE_SPECIFIC := b1
 
-PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Galaxy Nexus\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew & @MongooseHelix for updates\n------------------------------------------------\n"
+PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Galaxy Nexus\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew & @MongooseHelix for updates\nGet the latest rom at dl.evervolv.com\n------------------------------------------------\n"
 
-PRODUCT_PACKAGES += \
-    Camera
 
 # Hot reboot
 PRODUCT_PACKAGE_OVERLAYS += vendor/ev/overlay/hot_reboot
 
-# Copy compatible prebuilt files
+# Copy compatible bootanimation
 PRODUCT_COPY_FILES +=  \
     vendor/ev/prebuilt/720p/media/bootanimation.zip:system/media/bootanimation.zip
 
